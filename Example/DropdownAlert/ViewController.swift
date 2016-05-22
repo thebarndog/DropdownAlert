8cc55a70c2ebe05a4ae0ca53bd0501239c2c048c
Brendan Conron <conronb@gmail.com>
2016-05-22 10:44:47 -0400

    added demo

--- Example/DropdownAlert/ViewController.swift
+++ Example/DropdownAlert/ViewController.swift
@@ -7,18 +7,48 @@
 //
 
 import UIKit
+import DropdownAlert
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    // MARK: - Views
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var basicAnimationButton: UIButton!
+    @IBOutlet weak var springAnimationButton: UIButton!
+
+
 
 }
 
+// MARK: - UIViewController
+extension ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setup()
+    }
+
+
+
+}
+
+// MARK: - Setup
+private extension ViewController {
+    private func setup() {
+        DropdownAlert.defaultBackgroundColor = UIColor.blackColor()
+        DropdownAlert.defaultTextColor = UIColor.whiteColor()
+        self.basicAnimationButton.addTarget(self, action: #selector(showBasicAnimation), forControlEvents: .TouchUpInside)
+        self.springAnimationButton.addTarget(self, action: #selector(showSpringAnimation), forControlEvents: .TouchUpInside)
+    }
+}
+
+private extension ViewController {
+    @objc private func showBasicAnimation() {
+        DropdownAlert.showWithAnimation(.Basic(timingFunction: CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)), title: "Sweet demo!", message: "Look how cool this is!", duration: 2)
+    }
+
+    @objc private func showSpringAnimation() {
+        DropdownAlert.showWithAnimation(.Spring(bounce: 15, speed: 12), title: "Spring", message: "So bouncy!", duration: 2)
+    }
+
+}
